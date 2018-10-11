df1f7119169fa6f41c610a414216d33610c19931
Zac Ence <zac.ence@gmail.com>
2018-10-11 14:48:18 -0600

    Adding checkpoint for index files (ensuring files have been indexed)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Docker U54
+
+## Description
+
+* This container performs the `bwa mem` and `samtools view` commands within an isolated environment
+
+## Usage
--- run.sh
+++ run.sh
@@ -4,6 +4,15 @@ REF_GENOME=$1
 BWA_THREADS=$2
 SAMPLES=()
 
+#read -p "The reference does not contain the proper index files. Run bwa index? [y/n]" -n 1 -s to_index
+
+#if [ "$to_index" -eq "y" ]; then
+#    bwa index /data/ref_index/${REF_GENOME}
+#else
+#    echo "Index files for reference genome are required to run bwa"
+#    exit 1
+#fi
+
 for i in /data/SampleData/*; do
         SAMPLES+=($(echo ${i} | cut -d '/' -f 4 |  cut -d '.' -f 1))
 done
